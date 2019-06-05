6932b6d6c650f94e217eda209dcbe5b8cf6ec2bb
zxzxcv1123 <46100473+zxzxcv1123@users.noreply.github.com>
2019-06-05 16:29:34 +0800

    Update Day_044_HW_隨機森林.py

--- homework/Day_044_HW_隨機森林.py
+++ homework/Day_044_HW_隨機森林.py
@@ -12,6 +12,7 @@ metrics.accuracy_score(test_Y,pred) #0.9736
 print(clf.feature_importances_)
 
 #回歸
+#決策樹分數
 from sklearn.ensemble import RandomForestRegressor
 boston=datasets.load_boston()
 train_X,test_X,train_Y,test_Y=train_test_split(boston.data,boston.target,test_size=0.25, random_state=4)
@@ -22,3 +23,17 @@ pred=clf.predict(test_X)
 print(metrics.mean_absolute_error(test_Y,pred)) #2.6256
 #R2
 print(metrics.r2_score(test_Y,pred)) #0.8109
+#回歸樹
+from sklearn.tree import DecisionTreeRegressor
+clf=DecisionTreeRegressor()
+clf.fit(train_X,train_Y)
+pred=clf.predict(test_X)
+#MSE
+print(metrics.mean_absolute_error(test_Y,pred)) #3.3291
+#R2
+print(metrics.r2_score(test_Y,pred)) #0.7422
+
+#隨機森林分數大於決策樹
+
+
+
