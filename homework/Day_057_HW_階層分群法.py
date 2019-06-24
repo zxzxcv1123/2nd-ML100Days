39a0ec2571c3bbf5932881cd788756d890eec51e
zxzxcv1123 <46100473+zxzxcv1123@users.noreply.github.com>
2019-06-24 19:08:47 +0800

    Update Day_057_HW_階層分群法.py

--- homework/Day_057_HW_階層分群法.py
+++ homework/Day_057_HW_階層分群法.py
@@ -53,50 +53,6 @@ ax.dist = 12
 iris=datasets.load_iris()
 X=iris.data
 y=iris.target
-estimator=[('ward',AgglomerativeClustering(n_clusters=3,linkage='ward')),
-           ('complete',AgglomerativeClustering(n_clusters=3,linkage='complete')),
-           ('average',AgglomerativeClustering(n_clusters=3,linkage='average'))]
-fignum = 1
-titles = ['ward', 'complate', 'average']
-for name, est in estimators:
-    fig = plt.figure(fignum, figsize=(4, 3))
-    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-    
-    ## fit data
-    est.fit(X)
-    
-    labels = est.labels_
-
-    ax.scatter(X[:, 3], X[:, 0], X[:, 2],
-               c=labels.astype(np.float), edgecolor='k')
-
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-    ax.set_title(titles[fignum - 1])
-    ax.dist = 12
-    fignum = fignum + 1
-
-# Plot the ground truth
-fig = plt.figure(fignum, figsize=(4, 3))
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-
-for name, label in [('cls0', 0),
-                    ('cls1', 1),
-                    ('cls2', 2)]:
-    ax.text3D(X[y == label, 3].mean(),
-              X[y == label, 0].mean(),
-              X[y == label, 2].mean() + 2, name,
-              horizontalalignment='center',
-              bbox=dict(alpha=.2, edgecolor='w', facecolor='w'))
-# Reorder the labels to have colors matching the cluster results
-y = np.choose(y, [1, 2, 0]).astype(np.float)
-ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y, edgecolor='k')
-
-ax.w_xaxis.set_ticklabels([])
-ax.w_yaxis.set_ticklabels([])
-ax.w_zaxis.set_ticklabels([])
-ax.set_title('Ground Truth')
-ax.dist = 12
-
-#fig.show()
+est=AgglomerativeClustering(n_clusters=3,linkage='ward')
+est.fit(X)
+labels=est.labels_
